05da3eb6060e4905c8afa529e919f2391d85e4e4
felix01d <johnrodgers01d@gmail.com>
2020-08-16 13:26:54 -0500

    Updating size computation

--- Benchmark/npVpy_size.py
+++ Benchmark/npVpy_size.py
@@ -1,9 +1,29 @@
 import numpy as np
 import sys
 
+def get_size(obj, seen=None):
+    """Recursively finds size of objects"""
+    size = sys.getsizeof(obj)
+    if seen is None:
+        seen = set()
+    obj_id = id(obj)
+    if obj_id in seen:
+        return 0
+    # Important mark as seen *before* entering recursion to gracefully handle
+    # self-referential objects
+    seen.add(obj_id)
+    if isinstance(obj, dict):
+        size += sum([get_size(v, seen) for v in obj.values()])
+        size += sum([get_size(k, seen) for k in obj.keys()])
+    elif hasattr(obj, '__dict__'):
+        size += get_size(obj.__dict__, seen)
+    elif hasattr(obj, '__iter__') and not isinstance(obj, (str, bytes, bytearray)):
+        size += sum([get_size(i, seen) for i in obj])
+    return size
+
 if __name__ == '__main__':
     for power in range(0, 9):
         npdata = np.arange((10**power), dtype=np.float64)
         list_data = npdata.tolist()
-        print(f"np,{10**power},{sys.getsizeof(npdata)}")
-        print(f"py,{10**power},{sys.getsizeof(list_data)}")
+        print(f"np,{10**power},{npdata.nbytes}")
+        print(f"py,{10**power},{get_size(list_data)}")
